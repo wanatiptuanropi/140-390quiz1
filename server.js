379b5ac36d9a843a1c4fc08f49683339ca284a11
wanatip <s5930213049@phuket.psu.ac.th>
2018-09-17 10:47:07 +0700

    กำลัง แก้โค้ด Student

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var app = express();
 var mysql = require('mysql')
 var connection = mysql.createConnection({
   host     : 'www.db4free.net',
-  user     : ' s140390',
-  password : ' abc123**',
+  user     : 's140390',
+  password : 'abc123**',
   database : 'db140390'
 });
 app.set('view engine', 'ejs');
@@ -15,6 +15,22 @@ app.get('/', function (request, response)
     response.render('pages/index');
 
 });
+
+app.get('/student', function (request, response)
+{
+    connection.connect()
+
+    connection.query('select * from students', function (err, rows, fields) {
+      if (err) throw err
+    
+    response.render('pages/student');
+    })
+    
+    connection.end()
+
+
+});
+
 
 console.log('App is running at http://localhost:8080');
 app.listen(8080);
